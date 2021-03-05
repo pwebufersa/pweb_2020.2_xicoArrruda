c0e5b10e27baeb55b36039903628d1c995439c78
Programação WEB - UFERSA Angicos <pwebufersa@gmail.com>
2021-03-05 12:05:21 -0300

    Remove

--- demo/src/main/resources/import.sql
+++
@@ -1,4 +0,0 @@
-insert into contatos (id, nome, email) values (1, 'William Douglas', 'williamdouglas@algaworks.com');
-insert into contatos (id, nome, email) values (2, 'Al Ries', 'alries@algaworks.com');
-insert into contatos (id, nome, email) values (3, 'Mortimer J. Adler', 'mortimeradler@algaworks.com');
-insert into contatos (id, nome, email) values (4, 'Christian Barbosa', 'christianbarbosa@algaworks.com');
