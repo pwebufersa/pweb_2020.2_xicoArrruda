c52d89716e5fd40466d96569a197c491c3ee5cae
Programação WEB - UFERSA Angicos <pwebufersa@gmail.com>
2021-03-05 11:48:37 -0300

    Agenda em SpringBoot

---
+++ demo/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into contatos (id, nome, email) values (1, 'William Douglas', 'williamdouglas@algaworks.com');
+insert into contatos (id, nome, email) values (2, 'Al Ries', 'alries@algaworks.com');
+insert into contatos (id, nome, email) values (3, 'Mortimer J. Adler', 'mortimeradler@algaworks.com');
+insert into contatos (id, nome, email) values (4, 'Christian Barbosa', 'christianbarbosa@algaworks.com');
