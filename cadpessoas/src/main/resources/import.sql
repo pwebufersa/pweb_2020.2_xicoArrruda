3e00dbe94c64fae8a0a45a145f6993d1c1c07c12
Programação WEB - UFERSA Angicos <pwebufersa@gmail.com>
2021-03-26 22:24:51 -0300

    Delete

--- cadpessoas/src/main/resources/import.sql
+++
@@ -1,6 +0,0 @@
-﻿/*primera linha em branco*/
-INSERT INTO pessoas (nome, email, telefone, data_nascimento) VALUES ('Joaquim Maria Machado de Assis', 'machado@assis.com', '118354', parseDateTime('21/06/1839','dd/MM/yyyy'));
-INSERT INTO pessoas (nome, email, telefone, data_nascimento) VALUES ('Maria da Penha Maia Fernandes', 'mariaDaPenha@gov.br', '5555555', parseDateTime('01/02/1945','dd/MM/yyyy'));
-INSERT INTO pessoas (nome, email, telefone, data_nascimento) VALUES ('Pedro Álvares Cabral', 'pedrim@navio.br', '66666', parseDateTime('01/02/1467','dd/MM/yyyy'));
-INSERT INTO pessoas (nome, email, telefone, data_nascimento) VALUES ('Tarsila do Amaral', 'tarsila@abaporu.br', '66666', parseDateTime('01/09/1886','dd/MM/yyyy'));
-INSERT INTO pessoas (nome, email, telefone, data_nascimento) VALUES ('Oscar Niemeyer', 'obras@niemeyer.br', '66666', parseDateTime('15/12/1907','dd/MM/yyyy'));
